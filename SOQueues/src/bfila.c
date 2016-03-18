3ca9f12ec306645c2813eb49b6ef62e2014dc54e
fialves <fabio.alves.it@gmail.com>
2016-03-17 23:26:08 -0300

    7/10 tests OK

--- SOQueues/src/bfila.c
+++ SOQueues/src/bfila.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "fila2.h"
+#include "../include/fila2.h"
 
 int	vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
--- SOQueues/src/fila2.c
+++ SOQueues/src/fila2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include "fila2.h"
+#include "../include/fila2.h"
 
 #define ERROR_EMPTY_QUEUE -1
 #define ERROR_CREATION -1
 
-int	CreateFila2(PFILA2 pFila){
-    pFila = (FILA2*) calloc(sizeof(PFILA2));
+PFILA2 globalQueue;
+
+int     CreateFila2(PFILA2 pFila){
+    globalQueue = (PFILA2) malloc(sizeof(FILA2));
     if(pFila == NULL){
         return ERROR_CREATION;
     }
@@ -14,49 +16,62 @@ int	CreateFila2(PFILA2 pFila){
     pFila->last = NULL;
     pFila->it = NULL;
 
+    pFila = globalQueue;
+
     return 0;
 }
 
-int	FirstFila2(PFILA2 pFila){
-    if(pFila == NULL) {
+int     FirstFila2(PFILA2 pFila){
+    if(pFila->first == NULL) {
         return ERROR_EMPTY_QUEUE;
     }
     pFila->it = pFila->first;
     return 0;
 }
 
-int	LastFila2(PFILA2 pFila){
-    if(pFila == NULL){
+int     LastFila2(PFILA2 pFila){
+    if(pFila->last == NULL){
         return ERROR_EMPTY_QUEUE;
     }
     pFila->it = pFila->last;
     return 0;
 }
 
-int	NextFila2(PFILA2 pFila){
+int     NextFila2(PFILA2 pFila){
+    pFila->it = pFila->it->next;
     return 0;
 }
 
-void 	*GetAtIteratorFila2(PFILA2 pFila){
-    return pFila->it;
-}
-
-int	AppendFila2(PFILA2 pFila, void *content){
-    pFila->last->next = (NODE2*) malloc(sizeof(NODE2));
-    pFila->last->next->ant = pFila->last;
-    if(!FirstFila2(pFila)){
-        pFila->first = pFila->last;
+void    *GetAtIteratorFila2(PFILA2 pFila){
+    if(pFila != NULL){
+        return pFila->it->node;
     }
-    pFila->last = pFila->last->next;
-    pFila->last->node = content;
+    return NULL;
+}
+
+int     AppendFila2(PFILA2 pFila, void *content){
+    if(pFila->first == NULL){
+        pFila->first = (PNODE2) malloc(sizeof(NODE2));
+        pFila->first->node = content;
+        pFila->first->ant = NULL;
+        pFila->first->next = NULL;
+        pFila->last = pFila->first;
+        pFila->it = pFila->first;
+    }
+    else {
+        pFila->last->next = (PNODE2) malloc(sizeof(NODE2));
+        pFila->last->next->ant = pFila->last;
+        pFila->last = pFila->last->next;
+        pFila->last->node = content;
+    }
 
     return 0;
 }
 
-int	InsertAfterIteratorFila2(PFILA2 pFila, void *content){
+int     InsertAfterIteratorFila2(PFILA2 pFila, void *content){
     return 0;
 }
 
-int	DeleteAtIteratorFila2(PFILA2 pFila){
+int     DeleteAtIteratorFila2(PFILA2 pFila){
     return 0;
 }
